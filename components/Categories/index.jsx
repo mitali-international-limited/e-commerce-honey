1df220b56a041753104bda624e7621bbbd7a8251
munna <dev2.mitaliint@gmail.com>
2023-02-10 16:38:16 +0600

    dynamic category fuctionality in responsive

--- components/Categories/index.jsx
+++ components/Categories/index.jsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 import Card from "../Card";
+import categoryData from "../../utils/category-demo-data";
 
 const Categories = () => {
+  const [category, setCategory] = useState (categoryData)
+  console.log(category);
   return (
-    <section>
+    <section className="padding_inside">
       <h2 className="text-center">Top Categories</h2>
-      <div className="grid grid-rows-2">
-        <Card type="category" img="img" name="name" />
+      <div className="grid grid-cols-2 items-center gap-3 justify-between md:grid-cols-4">
+        
+        {
+          category.map((item, index) => {
+            return (
+              <Card
+                key={index}
+                name={item.name}
+                image={item.image}
+                
+              />
+            )
+          })
+        }
+
       </div>
     </section>
   );
