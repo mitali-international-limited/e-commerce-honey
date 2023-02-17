910be804adfbc457c82970195c1a6552f11978a1
munna <dev2.mitaliint@gmail.com>
2023-02-17 15:26:16 +0600

    manubar in header section

--- components/Categories/index.jsx
+++ components/Categories/index.jsx
@@ -7,27 +7,9 @@ import styles from "./style.module.css";
 const Categories = () => {
   const [category, setCategory] = useState(categoryData);
   return (
-
-    <section className="padding_inside">
-      <h2 className="mb-0 ">Top Categories</h2>
-      <hr className="h-px my-8 bg-secondary border-0 dark:bg-secondary" />
-      <div className="grid grid-cols-2 items-center gap-3 justify-between md:grid-cols-4">
-        {
-          category.map((item, index) => {
-            return (
-              <Card
-                key={index}
-                name={item.name}
-                image={item.image}
-                cardType="category"
-              />
-            )
-          })
-        }
-
     <section className={`padding_inside my-20`}>
       <h2 className="mb-0">Top Categories</h2>
-      <hr className="mx-0 mb-5" />
+      <hr className="h-px my-8 bg-secondary border-0 dark:bg-secondary" />
       <div className="grid grid-cols-2 items-center gap-3 justify-between p-6 md:grid-cols-4">
         {category.map((item, index) => {
           return (
