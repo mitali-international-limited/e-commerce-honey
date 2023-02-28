1630c1de32a30fe18d75256702f93dad7fccc759
Sm Ashraful <mitali.developer1@gmail.com>
2023-02-28 12:36:42 +0600

    product page demo create

---
+++ pages/product.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProductsPage = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-primary mt-24">
+      ProductsPage
+    </div>
+  );
+};
+
+export default ProductsPage;
