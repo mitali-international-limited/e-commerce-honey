0400dbfe1ba6fb20f8bd8ad6036323e091ec58aa
Sm Ashraful <mitali.developer1@gmail.com>
2023-02-28 14:30:16 +0600

    sign in design positioning

---
+++ pages/account.tsx
@@ -0,0 +1,24 @@
+// This is account section
+{
+  /*//This section contains:
+  * sign in
+  * sign up
+  * forget password 
+  * 
+  Author: Munna 
+*/
+}
+import React from "react";
+import Head from "next/head";
+
+import AuthAccount from "../components/Authorization/account";
+
+const Account = () => {
+  return (
+    <>
+      <AuthAccount />
+    </>
+  );
+};
+
+export default Account;
