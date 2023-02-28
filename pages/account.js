bf430823a8f6f5cc86709785ee78103d67ad51c4
Sm Ashraful <mitali.developer1@gmail.com>
2023-02-28 13:03:47 +0600

    account troublesooting

--- pages/account.js
+++ pages/account.js
@@ -1,3 +1,13 @@
+// This is account section
+{
+  /*//This section contains:
+  * sign in
+  * sign up
+  * forget password 
+  * 
+  Author: Munna 
+*/
+}
 import React from "react";
 
 import Head from "next/head";
@@ -105,21 +115,6 @@ const Account = () => {
                 </div>
               </div>
             </div>
-
-            {/* sign up section */}
-            {/* <div className="w-2/5 bg-secondary text-white rounded-tr-2xl rounded-br-2xl py-36 px-12">
-            <h2 className="text-3xl font-bold mb-2"> Hello</h2>
-            <div className="border-2 w-10 border-white inline-block mb-2"></div>
-            <p className="mb-10">
-              Fill up personal information and start journey with us
-            </p>
-            <a
-              href="#"
-              className="border-2 border-white rounded-full px-12 py-2 inline-block font-semibold hover:bg-white hover:text-secondary"
-            >
-              Sign Up
-            </a>
-          </div> */}
           </div>
         </main>
       </div>
