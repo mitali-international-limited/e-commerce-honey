6ee73caa0f579b155680ce66913850061a3027b6
Sm Ashraful <mitali.developer1@gmail.com>
2023-03-14 17:27:33 +0600

    troubleshoot cart page

--- pages/cart/cart.jsx
+++ pages/cart/cart.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Head from "next/head";
 import styles from "./Cart.module.css";
 import { MdClose } from "react-icons/md";
 import { AiOutlinePlus, AiOutlineMinus } from "react-icons/ai";
@@ -43,37 +44,25 @@ const tableData = [
   },
 ];
 
+const calculateTotalCost = () => {
+  let totalCost = 0;
+  tableData.forEach((item) => {
+    totalCost += item.total;
+  });
+  return totalCost;
+};
+
 export default function Cart() {
   const [data, setData] = useState(tableData);
-  const calculateTotalCost = () => {
-    let totalCost = 0;
-    tableData.forEach((item) => {
-      totalCost += item.total;
-    });
-    return totalCost;
-  };
+
   return (
     <div className="relative grid grid-cols-4 top-36 md:top-48 w-full px-4">
-       
-        <div className={`col-span-3 mx-4 py-10`}>
-          <h2 className="text-center font-bold text-secondary">Shopping Cart</h2>
+      <div className={`col-span-3 mx-4 py-10`}>
+        <h2 className="text-center font-bold text-secondary">Shopping Cart</h2>
 
-          <table className={`${styles.table}`}>
-            <thead>
-              <tr>
-                {tableHeader.map((header) => (
-                  <th className="p-2 text-center text-secondary" key={header}>
-                    {header}
-                  </th>
-                ))}
-
-    <div className="relative top-36 md:top-48">
-      <div className={styles.container}>
-        <h1 className="text-center font-bold">Shopping Cart</h1>
-
-        <table className={styles.table}>
+        <table className={`${styles.table}`}>
           <thead>
-            <tr className="">
+            <tr>
               {tableHeader.map((header) => (
                 <th className="p-2 text-center text-secondary" key={header}>
                   {header}
@@ -113,43 +102,10 @@ export default function Cart() {
                   ${item.total.toFixed(2)}
                 </td>
               </tr>
-            </thead>
-            <tbody>
-              {data.map((item) => (
-                <tr
-                  key={item.id}
-                  className="border border-l-0 border-r-0 border-gray"
-                >
-                  <td className="py-5 px-2 text-center flex justify-center items-center text-gray">
-                    <span className="mx-4 font-bold cursor-pointer hover:text-primary-red">
-                      <MdClose />
-                    </span>
-                    {item.close}
-                  </td>
-                  <td className="p-2 h-16 w-24 shadow-sm">
-                    <img className="w-full h-full bg-center" src={item.image} />
-                  </td>
-                  <td className="py-5 px-2 text-center">{item.product}</td>
-                  <td className="py-5 px-2 text-center">
-                    ${item.price.toFixed(2)}
-                  </td>
-                  <td className="py-5 px-2 text-center flex justify-center items-center">
-                    <span className="mx-4 font-bold cursor-pointer hover:bg-honey border border-gray">
-                      <AiOutlineMinus />
-                    </span>
-                    {item.quantity}
-                    <span className="mx-4 font-bold cursor-pointer hover:bg-honey border border-gray">
-                      <AiOutlinePlus />
-                    </span>
-                  </td>
-                  <td className="py-5 px-2 text-center pr-0">
-                    ${item.total.toFixed(2)}
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+            ))}
+          </tbody>
+        </table>
+      </div>
 
       <div className="col-span-1 my-10 border-gray border-l-2">
         <h2 className="text-center font-bold text-secondary">Cart Totals</h2>
@@ -158,19 +114,31 @@ export default function Cart() {
           <h1 className="font-bold">Shipping</h1>
           <hr className="h-px mb-5  bg-gray border-0 dark:bg-gray" />
           <label className=" items-center text-lg">
-            <input type="radio" name="shipping" className="form-radio h-5 w-5" />
+            <input
+              type="radio"
+              name="shipping"
+              className="form-radio h-5 w-5"
+            />
             <span className="ml-2 text-gray">
               Free Standard Shipping (5-8 Business Days)
             </span>
           </label>
           <label className=" items-center text-lg">
-            <input type="radio" name="shipping" className="form-radio h-5 w-5" />
+            <input
+              type="radio"
+              name="shipping"
+              className="form-radio h-5 w-5"
+            />
             <span className="ml-2 text-gray">
               Expedited Shipping (4 Business Days): $9.99
             </span>
           </label>
           <label className=" items-center text-lg">
-            <input type="radio" name="shipping" className="form-radio h-5 w-5" />
+            <input
+              type="radio"
+              name="shipping"
+              className="form-radio h-5 w-5"
+            />
             <span className="ml-2 text-gray">
               Express Shipping (2 to 3 Business Days): $17.99
             </span>
@@ -178,13 +146,10 @@ export default function Cart() {
           <hr className="h-px mb-5  bg-gray border-0 dark:bg-gray" />
         </div>
         <div className="font-bold w-full flex justify-between">
-            <p className="text-center ml-10 font-bold">
-              Total:
-            </p>
-            <p className="text-end text-secondary mx-4">
-              ${calculateTotalCost().toFixed(2)}
-            </p>
-
+          <p className="text-center ml-10 font-bold">Total:</p>
+          <p className="text-end text-secondary mx-4">
+            ${calculateTotalCost().toFixed(2)}
+          </p>
         </div>
       </div>
     </div>
